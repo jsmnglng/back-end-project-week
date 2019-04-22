d9bfb96bf856c743b86ad5bfe390531d7ee05fa6
Jasmine Galang <jsmn.glng@gmail.com>
2019-04-22 15:03:10 -0700

    Revert "Refactor how users are handled"

--- config/middleware.js
+++ config/middleware.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
-const logger = require("morgan");
 
 module.exports = server => {
   server.use(express.json());
   server.use(helmet());
   server.use(cors());
-  server.use(logger("dev"));
 };
--- database/migrations/20181210090058_create_notes_tbl.js
+++ database/migrations/20181210090058_create_notes_tbl.js
@@ -3,13 +3,6 @@ exports.up = function(knex, Promise) {
     tbl.increments("_id");
     tbl.string("title", 55).notNullable();
     tbl.string("textBody").notNullable();
-    tbl
-      .integer("user_id")
-      .references("id")
-      .inTable("users")
-      .onDelete("cascade")
-      .onUpdate("cascade")
-      .unsigned();
   });
 };
 
