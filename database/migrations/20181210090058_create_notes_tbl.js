6eda8a57c93e6f902031b1053c4002462a4808a5
jsmnglng <jsmn.glng@gmail.com>
2019-04-22 14:40:07 -0700

    Updated notes table

--- database/migrations/20181210090058_create_notes_tbl.js
+++ database/migrations/20181210090058_create_notes_tbl.js
@@ -3,6 +3,13 @@ exports.up = function(knex, Promise) {
     tbl.increments("_id");
     tbl.string("title", 55).notNullable();
     tbl.string("textBody").notNullable();
+    tbl
+      .integer("user_id")
+      .references("id")
+      .inTable("users")
+      .onDelete("cascade")
+      .onUpdate("cascade")
+      .unsigned();
   });
 };
 
